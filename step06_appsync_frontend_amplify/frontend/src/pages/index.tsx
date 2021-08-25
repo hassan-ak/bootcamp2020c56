bb4c2f61d37e7fb6be160b168aad0b2e297b335a
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-08-25 18:10:58 +0500

    Completed Step06 (Auto)

--- step06_appsync_frontend_amplify/frontend/src/pages/index.tsx
+++ step06_appsync_frontend_amplify/frontend/src/pages/index.tsx
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import shortid from "shortid";
+import { API } from "aws-amplify";
+import { addTodo } from "../graphql/mutations";
+import { getTodos } from "../graphql/queries";
+
+interface title {
+  title: string;
+}
+
+interface incomingData {
+  data: {
+    getTodos: title[];
+  };
+}
 
 export default function Home() {
-  return <div>Home Page</div>;
+  const [loading, setLoading] = useState(true);
+  const [todoData, setTodoData] = useState<incomingData | null>(null);
+  const todoTitleRef = useRef<any>("");
+
+  const fetchTodos = async () => {
+    try {
+      const data = await API.graphql({
+        query: getTodos,
+      });
+      setTodoData(data as incomingData);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const addTodoMutation = async () => {
+    try {
+      const todo = {
+        id: shortid.generate(),
+        title: todoTitleRef.current.value,
+        done: false,
+      };
+      const data = await API.graphql({
+        query: addTodo,
+        variables: {
+          todo: todo,
+        },
+      });
+      todoTitleRef.current.value = "";
+      fetchTodos();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        <div>
+          <label>
+            Todo: <input ref={todoTitleRef} />
+          </label>
+          <button onClick={() => addTodoMutation()}>Create Todo</button>
+          {todoData.data &&
+            todoData.data.getTodos.map((item, ind) => (
+              <div style={{ marginLeft: "1rem", marginTop: "2rem" }} key={ind}>
+                {item.title}
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
+  );
 }
