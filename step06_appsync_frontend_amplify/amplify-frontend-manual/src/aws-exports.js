0bdca807885c9f0ff665ace19600e51eb80ecaf6
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-10-30 03:04:39 +0500

    Initilaize Amplify in the app and run queries

---
+++ step06_appsync_frontend_amplify/amplify-frontend-manual/src/aws-exports.js
@@ -0,0 +1,10 @@
+const awsmobile = {
+  aws_project_region: "us-east-2",
+  aws_appsync_graphqlEndpoint:
+    "https://oeyqg3oukzcbbp7ydpv2faxt3q.appsync-api.us-east-2.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-2",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-7fwnqx5gbbdx7br7776vpw327y",
+};
+
+export default awsmobile;
--- step06_appsync_frontend_amplify/amplify-frontend-manual/src/pages/index.tsx
+++ step06_appsync_frontend_amplify/amplify-frontend-manual/src/pages/index.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { API } from "aws-amplify";
 
 export default function Home() {
+  const getTodos = `
+  query GetTodos{
+    getTodos{
+      id
+      title
+      done
+    }
+  }
+  `;
+
+  const fetchTodos = async () => {
+    try {
+      const data = await API.graphql({ query: getTodos });
+      console.log("Data : ", data);
+    } catch (error) {
+      console.log("Error : ", error);
+    }
+  };
+
+  fetchTodos();
+
   return <div>Home Page</div>;
 }
