5b93d6a72cf091a385d980c91d92a99eaf5614f9
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-10-29 12:15:12 +0500

    Delete Old files

--- step06_appsync_frontend_amplify/backend-cdk/functions/Todo.ts
+++
@@ -1,7 +0,0 @@
-type Todo = {
-  id: string;
-  title: string;
-  done: boolean;
-};
-
-export default Todo;
--- step06_appsync_frontend_amplify/frontend-manual/src/wrappers/wrap-root-element.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Amplify } from "aws-amplify";
-import awsmobile from "../aws-exports";
-
-export default ({ element }) => {
-  Amplify.configure(awsmobile);
-  return <div>{element}</div>;
-};
