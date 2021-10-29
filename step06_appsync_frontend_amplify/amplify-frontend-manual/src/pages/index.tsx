d59bf20028a8f82fc0310b2e659de65e4aa5bbe3
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-10-30 03:23:27 +0500

    Used mutation in the app

--- step06_appsync_frontend_amplify/amplify-frontend-manual/src/pages/index.tsx
+++ step06_appsync_frontend_amplify/amplify-frontend-manual/src/pages/index.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { API } from "aws-amplify";
+import shortid from "shortid";
 
 export default function Home() {
   const getTodos = `
-  query GetTodos{
-    getTodos{
-      id
-      title
-      done
+    query GetTodos{
+      getTodos{
+        id
+        title
+        done
+      }
     }
-  }
+  `;
+  const addTodo = `
+    mutation AddTodo($todo: TodoInput !) {
+      addTodo(todo: $todo) {
+        done
+        id
+        title
+      }
+    }
   `;
 
   const fetchTodos = async () => {
@@ -22,6 +32,27 @@ export default function Home() {
   };
 
   fetchTodos();
+  const Add_Todo = async () => {
+    try {
+      const todo = {
+        id: shortid.generate(),
+        title: "Hello from App",
+        done: false,
+      };
+      const data = await API.graphql({
+        query: addTodo,
+        variables: { todo: todo },
+      });
+      fetchTodos();
+    } catch (error) {
+      console.log("Error : ", error);
+    }
+  };
 
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button onClick={() => Add_Todo()}>Add Todo</button>
+    </div>
+  );
 }
