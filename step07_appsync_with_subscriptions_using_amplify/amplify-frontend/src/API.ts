e7347b047a98aa7552e546a78c7ef89ce759ae9b
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-10-31 03:30:18 +0500

    Amplify Initialization

---
+++ step07_appsync_with_subscriptions_using_amplify/amplify-frontend/src/API.ts
@@ -0,0 +1,46 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+
+export type TodoInput = {
+  title: string,
+  done: boolean,
+};
+
+export type Todo = {
+  __typename: "Todo",
+  id: string,
+  title: string,
+  done: boolean,
+};
+
+export type AddTodoMutationVariables = {
+  todo: TodoInput,
+};
+
+export type AddTodoMutation = {
+  addTodo:  {
+    __typename: "Todo",
+    id: string,
+    title: string,
+    done: boolean,
+  },
+};
+
+export type GetTodosQuery = {
+  getTodos?:  Array< {
+    __typename: "Todo",
+    id: string,
+    title: string,
+    done: boolean,
+  } | null > | null,
+};
+
+export type OnAddTodoSubscription = {
+  onAddTodo?:  {
+    __typename: "Todo",
+    id: string,
+    title: string,
+    done: boolean,
+  } | null,
+};
